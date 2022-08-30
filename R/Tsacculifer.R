6adcc25855a4289cddfdc2d4ba2625b744215a37
AniekeBrombacher <77287867+AniekeBrombacher@users.noreply.github.com>
2022-08-30 15:42:39 +0100

    Add files via upload

---
+++ R/Tsacculifer.R
@@ -0,0 +1,17 @@
+#' Example data to showcase the foram3D package functions
+#' 
+#' Contains chamber number, volume and centroid data of a 
+#' Trilobatus sacculifer specimen
+#' 
+#'@format A dataframe with 5 columns and 22 rows
+#' \describe{
+#'     \item{Chamber}{Chamber number}
+#'     \item{Volume}{Chamber volume}
+#'     \item{CentroidX}{X-coordinate of chamber centroid}
+#'     \item{CentroidY}{Y-coordinate of chamber centroid}
+#'     \item{CentroidZ}{Z-coordinate of chamber centroid}
+#'     }
+#'     
+#' @examples 
+#' data(Tsacculfier)
+"Tsacculifer"
