47a70142fc6c0cbd010ce8c32a8e78b1a642b267
AniekeBrombacher <77287867+AniekeBrombacher@users.noreply.github.com>
2022-08-31 10:05:35 +0100

    Add files via upload

--- R/check_chamber_order.R
+++ R/check_chamber_order.R
@@ -1,9 +1,9 @@
 #' Check if chambers are in the correct order
 #'
 #' This function checks if the chambers of a foram are in the correct order by analysing the 
-#' angles between consecutive chambers. In species with four or more chambers in the final whorl 
-#' the angles between consecutive chambers are expected to be larger than 90 degrees. The 
-#' exception is the angle between the first three chambers, which is closer to 60 degrees. In 
+#' angles between consecutive chambers. In species with three or more chambers in a whorl 
+#' the angles between consecutive chambers are expected to be larger than 60 degrees. The 
+#' exception is the angle between the first three chambers, which can be smaller than 60 degrees. In 
 #' specimens with the proloculus known to be present the first angle is not taken into 
 #' consideration. If the proloculus is not present due to for example dissolution, the first 
 #' angle can be taken into account.
@@ -24,7 +24,7 @@ check.chamber.order <- function(X, Y, Z, proloculus=TRUE)
   # Proloculus not present: analyse all chamber angles
   if (proloculus==FALSE)
   {
-    if(any( A[3:length(A)] < 90 ))
+    if(any( A[3:length(A)] < 60 ))
     {
       c <- rep('Check chamber order', length(A))
     }
@@ -37,7 +37,7 @@ check.chamber.order <- function(X, Y, Z, proloculus=TRUE)
   # Proloculus present: discard first angle
   else
   {
-    if(any( A[4:length(A)] < 90 ))
+    if(any( A[4:length(A)] < 60 ))
     {
       c <- rep('Check chamber order', length(A))
     }
